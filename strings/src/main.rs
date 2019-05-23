eb29bf08332af771b385dc15ef43132f3ed70123
Bertie <berniesnell@gmail.com>
2019-05-23 16:47:02 -0400

    look at format as an option that doesnt take ownerhsip of params

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -31,7 +31,15 @@ fn main() {
     // even though here the s parameter  is a &str, we can still add a &String in because Rust uses a DEREF COERCION
     // this turns &String into &str by COERCING with a &s2[..]
     // println!("{}", hello); // NOT VALID
+    // add does not take ownership os s, it will still be a valid String after this! (so the coercion is safe)
+    // the signature DOES take ownership over self (hello in the example above)
+    //
     println!("{}", hello_2);
+    let another_cutie = String::from("erm...");
+    let notTakingRef = String::from("thingy");
+    let testing_second_thing = another_cutie + &notTakingRef;
+    // can we now still use notTakingRef as it's a reference
+    println!("{}", notTakingRef);
 
     let mut mutable_string = String::from("Hello");
     let another_string = String::from("Let's go");
@@ -40,7 +48,17 @@ fn main() {
     mutable_string.push_str(&another_string);
     mutable_string.push_str(but_a_string_literal);
     println!("Let me still use it: {}, {}", another_string, but_a_string_literal);
-    // mutable_string.push("l");
+    mutable_string.push('l'); // uses single quotes now!
 
+    // all this get's pretty unweildy at some point
+    // let's use the format! macro :)
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let formatted_string = format!("{}-{}-{}", s1, s2, s3); // similar to println! but returns a new String
+    // easier to read!
+    // doesn't take ownership of it's parameters!
+    println!("{}", formatted_string);
 }
