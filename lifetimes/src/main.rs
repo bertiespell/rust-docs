4ed36fa51e1432b0504370aaaf2e82ca5106d610
Bertie <berniesnell@gmail.com>
2019-05-27 18:52:05 -0400

    looking at lifetime specifiers and making sure things are in scope at the right time - for borrow checker

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -6,12 +6,12 @@
 
 // Allows us to prevent dangling references
 fn main() {
-    let r;
+    // let r;
     {
         let x = 5;
-        r = &x // this won't compile - the value r is referring to has gone out of scope by the time we come to use it
+        // r = &x // this won't compile - the value r is referring to has gone out of scope by the time we come to use it
     }
-    println!("r: {}", r); // if this worked, r would be referencing memory that was deallocated when x went out of scope
+    // println!("r: {}", r); // if this worked, r would be referencing memory that was deallocated when x went out of scope
 
     // RUST KNOWS!!   .... because of the Borrow Checker ;)
 }
@@ -28,15 +28,57 @@ fn print_longest() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
+    // When we pass concrete references to longest, the concrete lifetime that is substituted for 'a is the part of the scope of x that overlaps with the scope of y. In other words, the generic lifetime 'a will get the concrete lifetime that is equal to the smaller of the lifetimes of x and y. 
+
+    // i.e. Rust can now infer this, we don't need to make this concrete here, because it just takes the smallest of the two
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
 
-fn longest(x: &str, y: &str) -> &str {
+// When we’re defining this function, we don’t know the concrete values that will be passed into this function, so we don’t know whether the if case or the else case will execute. We also don’t know the concrete lifetimes of the references that will be passed in, so we can’t look at the scopes
+
+// The borrow checker can’t determine this either, because it doesn’t know how the lifetimes of x and y relate to the lifetime of the return value. To fix this error, we’ll add generic lifetime parameters that define the relationship between the references so the borrow checker can perform its analysis.
+
+// Basically, we need to add more information for the borrow checker
+
+// lifetime annotation syntax
+// functions can accept references with any lifetime by specifying a generic lifetime parameter.
+
+// Lifetimes have a very particular type of syntax
+
+// The names of lifetime parameters must start with an apostrophe (') and are usually all lowercase and very short, like generic types
+// Most people use the name 'a
+
+// Here we can place lifetime parameter annotations after the & of a reference, using a space to separate the reference from the annotations type
+
+// as with generic types, generic lifetime specifiers need to be in angled brackets
+
+// The constraint we want to express in this signature is that all the references in the parameters and the return value must have the same lifetime.
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str { // The function signature also tells Rust that the string slice returned from the function will live at least as long as lifetime 'a. 
+    // That's what we want Rust to enforce
     if x.len() > y.len() {
         return x
     } else {
         return y
     }
+}
+
+fn example_lifetimes_1() {
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result); // the shorter lifetime is used, but this is valid in here so it's fine
+    }
+}
+
+fn example_lifetimes_2() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str()); // this won't compile - "string2 doesn't live long enough"
+    }
+        println!("The longest string is {}", result);
 }
 
