0e6d21b0c62f8dfc9f1100ec6df1ea3bb600e486
Bertie <berniesnell@gmail.com>
2019-05-28 13:12:31 -0400

    test more specific panic situations by comparing error messages

--- testing/adder/src/lib.rs
+++ testing/adder/src/lib.rs
@@ -93,9 +93,14 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value should be between 1 and 100, got {}", value);
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.",
+                   value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.",
+                   value);
         }
+
         Guess {
             value,
         }
@@ -108,6 +113,21 @@ mod tests_again {
 
     #[test]
     #[should_panic] // need this attribute to test that it panics!
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+// Tests that use should_panic can be imprecise because they only indicate that the code has caused some panic. A should_panic test would pass even if the test panics for a different reason from the one we were expecting to happen. To make should_panic tests more precise, we can add an optional expected parameter to the should_panic attribute. The test harness will make sure that the failure message contains the provided text.
+
+// Let's test more specific panic cases
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
