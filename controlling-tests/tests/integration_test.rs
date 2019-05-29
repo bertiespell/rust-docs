699575f9fd24a9546630ef960052a4708e3a3995
Bertie <berniesnell@gmail.com>
2019-05-29 10:32:26 -0400

    test helper code

--- controlling-tests/tests/integration_test.rs
+++ controlling-tests/tests/integration_test.rs
@@ -4,3 +4,7 @@ use adder;
 fn it_adds_twoooooo() {
     assert_eq!(4, adder::add_two(2));
 }
+
+// for test helper code (which we don't want to run)
+
+// To avoid having common appear in the test output, instead of creating tests/common.rs, we’ll create tests/common/mod.rs. This is an alternate naming convention that Rust also understa
