431085e6560df24999e8c9dd7ac3d3818eda887d
Bertie <berniesnell@gmail.com>
2019-06-12 18:30:51 -0400

    using a struct to cache data and pattern match on lazy loaded Result evaluation

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -28,7 +28,36 @@ struct Cacher<T>
 {
     calculation: T, // holds a closure in T
     value: Option<u32>, // the resulting (cached) calculation
+    // Before we execute the closure, value will be None
 }
+
+impl<T> Cacher<T> 
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 { // We want Cacher to manage the struct fields’ values rather than letting the calling code potentially change the values in these fields directly, so these fields are private.
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+/**
+ When code using a Cacher asks for the result of the closure, the Cacher will execute the closure at that time and store the result within a Some variant in the value field. 
+ */
+
+// Functions can implement all three of the Fn traits too. If what we want to do doesn’t require capturing a value from the environment, we can use a function rather than a closure where we need something that implements an Fn trait.
 
 // This code works the way the business wants it to now, but let’s say the data science team decides that we need to make some changes to the way we call the simulated_expensive_calculation function in the future. To simplify the update when those changes happen, we want to refactor this code so it calls the simulated_expensive_calculation function only once. We also want to cut the place where we’re currently unnecessarily calling the function twice without adding any other calls to that function in the process. That is, we don’t want to call it if the result isn’t needed, and we still want to call it only once.
 
@@ -42,20 +71,26 @@ fn generate_workout(intensity: u32, random_number: u32) {
 
     // We can actually move the whole body of simulated_expensive_calculation within the closure we’re introducing here:
 
-    let expensive_closure = |num| { // type inferance => Closures don’t require you to annotate the types of the parameters or the return value like fn functions do. Type annotations are required on functions because they’re part of an explicit interface exposed to your users. Defining this interface rigidly is important for ensuring that everyone agrees on what types of values a function uses and returns. But closures aren’t used in an exposed interface like this: they’re stored in variables and used without naming them and exposing them to users of our library.
+    let expensive_closure = |num: u32| { // type inferance => Closures don’t require you to annotate the types of the parameters or the return value like fn functions do. Type annotations are required on functions because they’re part of an explicit interface exposed to your users. Defining this interface rigidly is important for ensuring that everyone agrees on what types of values a function uses and returns. But closures aren’t used in an exposed interface like this: they’re stored in variables and used without naming them and exposing them to users of our library.
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     }; // Note that this let statement means expensive_closure contains the definition of an anonymous function, not the resulting value of calling the anonymous function. Recall that we’re using a closure because we want to define the code to call at one point, store that code, and call it at a later point; the code we want to call is now stored in expensive_closure.
 
+    let mut expensive_cached_closure = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure(intensity) // replaced the old (non-closure way) => simulated_expensive_calculation(intensity)
+            expensive_cached_closure.value(intensity) // replaced the old => expensive_closure(intensity). Which in turn replaced the even older (non-closure way) => simulated_expensive_calculation(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity) // Low-intensity workout plans will recommend a number of push-ups and sit-ups based on the complex algorithm we’re simulating.
+            expensive_cached_closure.value(intensity) // Low-intensity workout plans will recommend a number of push-ups and sit-ups based on the complex algorithm we’re simulating.
 
             // we’re still calling the closure twice in the first if block, which will call the expensive code twice and make the user wait twice as long as they need to. We could fix this problem by creating a variable local to that if block to hold the result of calling the closure, but closures provide us with another solution.
 
@@ -67,7 +102,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_cached_closure.value(intensity)
             );
         }
     }
