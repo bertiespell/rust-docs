ffc6cdfb7f4791c85391527cb206eb6ad643b1b3
Bertie <berniesnell@gmail.com>
2019-06-13 18:53:33 -0400

    let's count the references as they go out of scope

--- drop_trait/src/main.rs
+++ drop_trait/src/main.rs
@@ -12,6 +12,8 @@ fn main() {
     println!("CustomSmartPointer created.");
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    actually_count_references();
 }
 
 struct CustomSmartPointer {
@@ -86,4 +88,16 @@ fn use_reference_count_list() {
 
     // ======== NOTATION ========
     // We could have called a.clone() rather than Rc::clone(&a), but Rust’s convention is to use Rc::clone in this case. The implementation of Rc::clone doesn’t make a deep copy of all the data like most types’ implementations of clone do. The call to Rc::clone only increments the reference count, which doesn’t take much time. Deep copies of data can take a lot of time. By using Rc::clone for reference counting, we can visually distinguish between the deep-copy kinds of clones and the kinds of clones that increase the reference count. When looking for performance problems in the code, we only need to consider the deep-copy clones and can disregard calls to Rc::clone.
+}
+
+fn actually_count_references() {
+    let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = RcCons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = RcCons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
