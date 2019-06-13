394d24df357abfe0d8b9c8fbc45b18e4cd698d3b
Bertie <berniesnell@gmail.com>
2019-06-13 17:37:29 -0400

    Dropping early

--- drop_trait/src/main.rs
+++ drop_trait/src/main.rs
@@ -4,7 +4,14 @@
 fn main() {
     let c = CustomSmartPointer { data: String::from("my stuff") };
     let d = CustomSmartPointer { data: String::from("other stuff") };
-    println!("CustomSmartPointers created."); // When main finishes - these will go out of scope and be dropped - printing out the line below declared in the trait
+    println!("CustomSmartPointers created."); // When main finishes - these will go out of scope and be dropped - printing out the line below declared in the trait... Variables are dropped in the reverse order of their creation, so d was dropped before c. 
+
+    // Dropping early - included in the prelude
+
+    let c = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
 struct CustomSmartPointer {
@@ -16,3 +23,12 @@ impl Drop for CustomSmartPointer {
         println!("Dropping: {}", self.data);
     }
 }
+
+// ====== Calling Drop Early ======
+
+// Occasionally, however, you might want to clean up a value early. One example is when using smart pointers that manage locks: you might want to force the drop method that releases the lock to run so other code in the same scope can acquire the lock.
+
+// Rust doesn’t let you call the Drop trait’s drop method manually; instead you have to call the std::mem::drop function provided by the standard library if you want to force a value to be dropped before the end of its scope.
+
+// Rust doesn’t let us call drop explicitly because Rust would still automatically call drop on the value at the end of main. This would be a double free error because Rust would be trying to clean up the same value twice.
+
