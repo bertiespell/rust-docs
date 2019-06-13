58c2b70f23203886df910924e1bd704022f0e41a
Bertie <berniesnell@gmail.com>
2019-06-13 17:31:29 -0400

    implement custom drop trait

--- drop_trait/src/main.rs
+++ drop_trait/src/main.rs
@@ -1,3 +1,18 @@
+// The second trait important to the smart pointer pattern is Drop, which lets you customize what happens when a value is about to go out of scope. You can provide an implementation for the Drop trait on any type, and the code you specify can be used to release resources like files or network connections. We’re introducing Drop in the context of smart pointers because the functionality of the Drop trait is almost always used when implementing a smart pointer. For example, Box<T> customizes Drop to deallocate the space on the heap that the box points to.
+
+// Specify the code to run when a value goes out of scope by implementing the Drop trait. The Drop trait requires you to implement one method named drop that takes a mutable reference to self. 
 fn main() {
-    println!("Hello, world!");
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created."); // When main finishes - these will go out of scope and be dropped - printing out the line below declared in the trait
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping: {}", self.data);
+    }
 }
