8928afb69bebe3a17e1b999041657f481b188c37
Bertie <berniesnell@gmail.com>
2019-06-15 20:02:42 -0400

    Different patterns around borrowing and compile vs runtime checks

---
+++ interior_mutability/src/main.rs
@@ -0,0 +1,28 @@
+// Interior mutability is a design pattern in Rust that allows you to mutate data even when there are immutable references to that data; normally, this action is disallowed by the borrowing rules. To mutate data, the pattern uses unsafe code inside a data structure to bend Rust’s usual rules that govern mutation and borrowing.
+
+// Unlike Rc<T>, the RefCell<T> type represents single ownership over the data it holds. So, what makes RefCell<T> different from a type like Box<T>? Recall the borrowing rules you learned in Chapter 4:
+
+// At any given time, you can have either (but not both of) one mutable reference or any number of immutable references.
+// References must always be valid.
+
+// With references and Box<T>, the borrowing rules’ invariants are enforced at compile time. With RefCell<T>, these invariants are enforced at runtime. With references, if you break these rules, you’ll get a compiler error. With RefCell<T>, if you break these rules, your program will panic and exit.
+
+// The advantage of checking the borrowing rules at runtime instead is that certain memory-safe scenarios are then allowed, whereas they are disallowed by the compile-time checks. Static analysis, like the Rust compiler, is inherently conservative. Some properties of code are impossible to detect by analyzing the code: the most famous example is the Halting Problem, which is beyond the scope of this book but is an interesting topic to research.
+
+// The RefCell<T> type is useful when you’re sure your code follows the borrowing rules but the compiler is unable to understand and guarantee that.
+
+// Similar to Rc<T>, RefCell<T> is only for use in single-threaded scenarios and will give you a compile-time error if you try using it in a multithreaded context.
+
+// The different scenarios
+
+/**
+ * Here is a recap of the reasons to choose Box<T>, Rc<T>, or RefCell<T>:
+
+Rc<T> enables multiple owners of the same data; Box<T> and RefCell<T> have single owners.
+Box<T> allows immutable or mutable borrows checked at compile time; Rc<T> allows only immutable borrows checked at compile time; RefCell<T> allows immutable or mutable borrows checked at runtime.
+Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
