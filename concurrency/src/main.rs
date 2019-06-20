2d911c5c101a36f420ac42dc37a41d6b69cc1649
Bertie <berniesnell@gmail.com>
2019-06-20 17:25:23 -0400

    Use move to pass ownership between threads in closures

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main2() {
-    let handle = thread::spawn(|| {
+    let handle = thread::spawn(|| { // we’re not using any data from the main thread in the spawned thread’s code (no args to the closure)... To use data from the main thread in the spawned thread, the spawned thread’s closure must capture the values it needs. 
         for i in 1..10 { // this typically only gets to 5 before the main thread shut down (no guarantee)
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1)); // The calls to thread::sleep force a thread to stop its execution for a short duration, allowing a different thread to run.. The threads will probably take turns, but that isn’t guaranteed: it depends on how your operating system schedules the threads.
@@ -19,7 +19,7 @@ fn main2() {
 }
 
 // Switch this to the MAIN MAIN method to see handle block the thread - print to 9, BEFORE the next counting starts
-fn main() {
+fn main1() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
@@ -33,4 +33,16 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || { // implicit captures -  we force the closure to take ownership of the values it’s using rather than allowing Rust to infer that it should borrow the values.
+        println!("Here's a vector: {:?}", v); // without V being moved it errors... Rust infers how to capture v, and because println! only needs a reference to v, the closure tries to borrow v. However, there’s a problem: Rust can’t tell how long the spawned thread will run, so it doesn’t know if the reference to v will always be valid.
+    });
+
+    // drop(v); // WITHOUT move || in the closure above => oh no! This causes some serious issues... WITH move || in the closure => Compiler error, v was moved above :)
+
+    handle.join().unwrap();
 }
