a5aff15ff6104e977b2528357b01ee3c0325bd38
Bertie <berniesnell@gmail.com>
2019-06-20 15:31:10 -0400

    looking into case for weak references, in graph structures

--- graphs/src/main.rs
+++ graphs/src/main.rs
@@ -7,6 +7,10 @@ use std::cell::RefCell;
 struct Node {
     value: i32,
     children: RefCell<Vec<Rc<Node>>> // We want a Node to own its children, and we want to share that ownership with variables so we can access each Node in the tree directly. To do this, we define the Vec<T> items to be values of type Rc<Node>. We also want to modify which nodes are children of another node, so we have a RefCell<T> in children around the Vec<Rc<Node>>.
+
+    // To make the child node aware of its parent, we need to add a parent field to our Node struct definition. The trouble is in deciding what the type of parent should be. We know it can’t contain an Rc<T>, because that would create a reference cycle with leaf.parent pointing to branch and branch.children pointing to leaf, which would cause their strong_count values to never be 0...RefCell
+
+    // Thinking about the relationships another way, a parent node should own its children: if a parent node is dropped, its child nodes should be dropped as well. However, a child should not own its parent: if we drop a child node, the parent should still exist. This is a case for weak references!
 }
 
 fn main() {
