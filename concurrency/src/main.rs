719f302439985aed3f1fb475eb7c8a4426f37eff
Bertie <berniesnell@gmail.com>
2019-06-20 17:04:40 -0400

    how to finish a thread with join

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 { // this typically only gets to 5 before the main thread shut down (no guarantee)
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1)); // The calls to thread::sleep force a thread to stop its execution for a short duration, allowing a different thread to run.. The threads will probably take turns, but that isn’t guaranteed: it depends on how your operating system schedules the threads.
@@ -13,4 +13,7 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    // A JoinHandle is an owned value that, when we call the join method on it, will wait for its thread to finish
+    handle.join().unwrap();
 }
