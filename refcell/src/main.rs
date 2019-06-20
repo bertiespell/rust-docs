79ee4f1fcacaf5efe967d3b70fd8daaa8c739a15
Bertie <berniesnell@gmail.com>
2019-06-20 14:10:12 -0400

    looking into when refcells panic - and runtime borrow checking rules

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -59,6 +59,18 @@ mod tests {
         }
     }
 
+    // --------- EXAMPLE: Here we try to create two mutable borrows (against the borrowing rules) - if this block were implemented instead, the tests below would PANIC! ----------------
+    
+    // impl Messenger for MockMessenger {
+    //     fn send(&self, message: &str) {
+    //         let mut one_borrow = self.sent_messages.borrow_mut();
+    //         let mut two_borrow = self.sent_messages.borrow_mut();
+
+    //         one_borrow.push(String::from(message));
+    //         two_borrow.push(String::from(message));
+    //     }
+    // }
+
     #[test]
     fn it_sends_an_over_75_percent_warning_message() {
         let mock_messenger = MockMessenger::new();
