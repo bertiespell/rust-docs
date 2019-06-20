b90a942f285c5222892aae79466bc0d15ad9cc56
Bertie <berniesnell@gmail.com>
2019-06-20 14:41:22 -0400

    creating reference cycle

--- reference_cycle/src/main.rs
+++ reference_cycle/src/main.rs
@@ -2,8 +2,29 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use List::{Cons, Nil};
 
+// This code creates a list in a and a list in b that points to the list in a. Then it modifies the list in a to point to b, creating a reference cycle. There are println! statements along the way to show what the reference counts are at various points in this process.
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
 
 #[derive(Debug)]
