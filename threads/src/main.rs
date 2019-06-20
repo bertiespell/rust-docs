2cbfec458132384d56452fa7629ab041248676a9
Bertie <berniesnell@gmail.com>
2019-06-20 19:42:43 -0400

    Difference between recv and try_recv when waiting for values and wanting non-blocking code

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         tx.send(val).unwrap(); // The transmitting end has a send method that takes the value we want to send. The send method returns a Result<T, E> type, so if the receiving end has already been dropped and there’s nowhere to send a value, the send operation will return an error. In this example, we’re calling unwrap to panic in case of an error. But in a real application, we would handle it properly
     });
 
-    let received = rx.recv().unwrap();
+    let received = rx.recv().unwrap(); // The receiving end of a channel has two useful methods: recv and try_recv. We’re using recv, short for receive, which will block the main thread’s execution and wait until a value is sent down the channel. Once a value is sent, recv will return it in a Result<T, E>. When the sending end of the channel closes, recv will return an error to signal that no more values will be coming.
+
+    /**
+     * The try_recv method doesn’t block, but will instead return a Result<T, E> immediately: an Ok value holding a message if one is available and an Err value if there aren’t any messages this time. Using try_recv is useful if this thread has other work to do while waiting for messages: we could write a loop that calls try_recv every so often, handles a message if one is available, and otherwise does other work for a little while until checking again.
+     */
     println!("Got: {}", received);
 }
