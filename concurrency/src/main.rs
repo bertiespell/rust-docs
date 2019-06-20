ebb1c4685a458032e02e7d08080f2a2513490c6d
Bertie <berniesnell@gmail.com>
2019-06-20 16:58:41 -0400

    notes about sleeping and scheduling in threads

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_millis(1)); // The calls to thread::sleep force a thread to stop its execution for a short duration, allowing a different thread to run.. The threads will probably take turns, but that isn’t guaranteed: it depends on how your operating system schedules the threads.
         }
     });
 
