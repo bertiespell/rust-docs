8000fb3b7a2eaa9c30065e7c43a71790c78b9da2
Bertie <berniesnell@gmail.com>
2019-06-20 20:10:05 -0400

    multiple threads and channels - nondeterministic message passing

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -33,7 +33,7 @@ fn main2() {
 
 use std::time::Duration;
 
-fn main() {
+fn main3() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
@@ -55,4 +55,45 @@ fn main() {
     }
 
     // Because we don’t have any code that pauses or delays in the for loop in the main thread, we can tell that the main thread is waiting to receive values from the spawned thread.
+}
+
+fn main() {
+    multiple_transmitters();
+}
+
+fn multiple_transmitters() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx { // these come through non-deterministically!
+        println!("Got: {}", received);
+    }
 }
