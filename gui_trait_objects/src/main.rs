2cd8c99c29954b227377b3d90ac07b57306ff23e
Bertie <berniesnell@gmail.com>
2019-06-22 13:51:11 -0400

    important note about the difference between trait bounds and trait objects

--- gui_trait_objects/src/main.rs
+++ gui_trait_objects/src/main.rs
@@ -13,7 +13,7 @@ pub struct Screen {
 impl Screen {
     pub fn run(&self) {
         for component in self.components.iter() {
-            component.draw();
+            component.draw(); // This works differently than defining a struct that uses a generic type parameter with trait bounds... A generic type parameter can only be substituted with one concrete type at a time, whereas trait objects allow for multiple concrete types to fill in for the trait object at runtime.
         }
     }
 }
