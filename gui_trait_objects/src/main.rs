b5229bb0864c8d31b7af8ad019bc0e7769754e93
Bertie <berniesnell@gmail.com>
2019-06-22 13:52:51 -0400

    example code with trait bounds

--- gui_trait_objects/src/main.rs
+++ gui_trait_objects/src/main.rs
@@ -17,3 +17,24 @@ impl Screen {
         }
     }
 }
+
+/**
+ * E.G. THIS =>
+ * 
+
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+    where T: Draw {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+======> Would restrict us to a Screen instance that has a list of components all of type Button or all of type TextField. If you’ll only ever have homogeneous collections, using generics and trait bounds is preferable because the definitions will be monomorphized at compile time to use the concrete types.
+
+ */
