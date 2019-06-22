ea3cf76644d750effc039527687951f03ca52474
Bertie <berniesnell@gmail.com>
2019-06-22 19:19:50 -0400

    Adding a library file - main file tests functality of state transitions

--- state_pattern_oo/src/main.rs
+++ state_pattern_oo/src/main.rs
@@ -1,3 +1,24 @@
+/** Task: 
+ * 
+1. A blog post starts as an empty draft.
+2. When the draft is done, a review of the post is requested.
+3. When the post is approved, it gets published.
+4. Only published blog posts return content to print, so unapproved posts can’t accidentally be published
+
+Any other changes attempted on a post should have no effect. For example, if we try to approve a draft blog post before we’ve requested a review, the post should remain an unpublished draft.
+ */
+extern crate state_pattern_oo;
+use state_pattern_oo::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
