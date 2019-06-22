b81e7352fbc74a4262f2e75135e45f199d5d2af6
Bertie <berniesnell@gmail.com>
2019-06-22 12:51:28 -0400

    using pub and private methods and functions to encapsulate data and provide API

--- object_orientated/src/main.rs
+++ object_orientated/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub struct AveragedCollection {
+    list: Vec<i32>, // these stay private - we update them with the methods below
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            },
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
