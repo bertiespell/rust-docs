ef302e831c4b0b437110461efc7372219515ae45
Bertie <berniesnell@gmail.com>
2019-06-22 18:25:34 -0400

    implementing the trait

--- gui_trait_objects/src/main.rs
+++ gui_trait_objects/src/main.rs
@@ -38,3 +38,17 @@ impl<T> Screen<T>
 ======> Would restrict us to a Screen instance that has a list of components all of type Button or all of type TextField. If you’ll only ever have homogeneous collections, using generics and trait bounds is preferable because the definitions will be monomorphized at compile time to use the concrete types.
 
  */
+
+// Now we’ll add some types that implement the Draw trai
+
+pub struct Button {
+    pub width: i32,
+    pub height: i32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to draw a button would go here.
+    }
+}
