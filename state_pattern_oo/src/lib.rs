f1ab18264d58ba67d7cc8305d86a5bcbb8d2a8ef
Bertie <berniesnell@gmail.com>
2019-06-22 19:29:45 -0400

    method to update content of blog post

--- state_pattern_oo/src/lib.rs
+++ state_pattern_oo/src/lib.rs
@@ -18,6 +18,10 @@ impl Post {
             content: String::new(),
         }
     }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
 }
 
 trait State {}
