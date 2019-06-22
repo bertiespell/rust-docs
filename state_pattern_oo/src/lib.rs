36728263de5d29c99d7604d45bfc71ae45ff3933
Bertie <berniesnell@gmail.com>
2019-06-22 19:26:08 -0400

    notes

--- state_pattern_oo/src/lib.rs
+++ state_pattern_oo/src/lib.rs
@@ -1,3 +1,11 @@
+/**
+
+The State trait defines the behavior shared by different post states, and the Draft, PendingReview, and Published states will all implement the State trait. For now, the trait doesn’t have any methods, and we’ll start by defining just the Draft state because that is the state we want a post to start in.
+
+When we create a new Post, we set its state field to a Some value that holds a Box. This Box points to a new instance of the Draft struct. This ensures whenever we create a new instance of Post, it will start out as a draft. Because the state field of Post is private, there is no way to create a Post in any other state! In the Post::new function, we set the content field to a new, empty String.
+
+ */
+
 pub struct Post {
     state: Option<Box<dyn State>>,
     content: String,
