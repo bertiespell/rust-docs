36751b3991e9f00ae90746d6c0d9a8ae11d0e943
Bertie <berniesnell@gmail.com>
2019-06-23 20:26:28 -0400

    notes on pattern matching

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -1,3 +1,16 @@
+/**
+ * Patterns are a special syntax in Rust for matching against the structure of types, both complex and simple. Using patterns in conjunction with match expressions and other constructs gives you more control over a program’s control flow. A pattern consists of some combination of the following:
+
+Literals
+Destructured arrays, enums, structs, or tuples
+Variables
+Wildcards
+Placeholders
+These components describe the shape of the data we’re working with, which we then match against values to determine whether our program has the correct data to continue running a particular piece of code.
+
+To use a pattern, we compare it to some value. If the pattern matches the value, we use the value parts in our code
+
+ */
 fn main() {
     println!("Hello, world!");
 }
