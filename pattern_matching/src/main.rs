df82af7c9a39231a0a767fac3838342193b07a03
Bertie <berniesnell@gmail.com>
2019-06-23 20:36:59 -0400

    pattern matching in a for loop

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -56,4 +56,11 @@ fn main() {
     while let Some(top) = stack.pop() {
         println!("{}", top);
     }
+
+    // for loops - In a for loop, the pattern is the value that directly follows the keyword for, so in for x in y the x is the pattern.
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
 }
