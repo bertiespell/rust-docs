d6774b056e7a3901f0183b0a366ce54a371b1e06
Bertie <berniesnell@gmail.com>
2019-06-25 18:12:20 -0400

    initial notes on refutability

--- refutability/src/main.rs
+++ refutability/src/main.rs
@@ -1,3 +1,5 @@
+// Patterns come in two forms: refutable and irrefutable. Patterns that will match for any possible value passed are irrefutable. An example would be x in the statement let x = 5; because x matches anything and therefore cannot fail to match. Patterns that can fail to match for some possible value are refutable. An example would be Some(x) in the expression if let Some(x) = a_value because if the value in the a_value variable is None rather than Some, the Some(x) pattern will not match.
+
 fn main() {
     println!("Hello, world!");
 }
