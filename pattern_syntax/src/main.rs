428bd703d80d882b6c9a67a071346a2d094b3be9
Bertie <berniesnell@gmail.com>
2019-06-25 19:05:56 -0400

    Notes on bindings

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -344,6 +344,15 @@ enum Message3 {
     Hello { id: i32 },
 }
 
+/**
+This example will print Found an id in range: 5. By specifying id_variable @ before the range 3...7, we’re capturing whatever value matched the range while also testing that the value matched the range pattern.
+
+In the second arm, where we only have a range specified in the pattern, the code associated with the arm doesn’t have a variable that contains the actual value of the id field. The id field’s value could have been 10, 11, or 12, but the code that goes with that pattern doesn’t know which it is. The pattern code isn’t able to use the value from the id field, because we haven’t saved the id value in a variable.
+
+In the last arm, where we’ve specified a variable without a range, we do have the value available to use in the arm’s code in a variable named id. The reason is that we’ve used the struct field shorthand syntax. But we haven’t applied any test to the value in the id field in this arm, as we did with the first two arms: any value would match this pattern.
+
+Using @ lets us test a value and save it in a variable within one pattern.
+ */
 fn using_bindings() {
     let msg = Message3::Hello { id: 5 };
 
