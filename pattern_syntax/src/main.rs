0fbb9cdd45b8fbe691643685658dcbc1c1c74076
Bertie <berniesnell@gmail.com>
2019-06-25 18:40:19 -0400

    Enum matching can be nested too!

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     destructuring();
     use_literal_destructuring();
     destructure_enum();
+    nested_enum_matching();
 }
 
 fn match_against_literals() {
@@ -125,5 +126,43 @@ fn destructure_enum() {
                 b
             )
         }
+    }
+}
+
+// NESTED destructuring - Matching can work on nested structures too!
+
+enum Color {
+   Rgb(i32, i32, i32),
+   Hsv(i32, i32, i32)
+}
+
+enum NestedMessage {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn nested_enum_matching() {
+    let msg = NestedMessage::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        NestedMessage::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )     
+        },
+        NestedMessage::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h,
+                s,
+                v
+            )
+        }
+        _ => ()
     }
 }
