9a4a7d8d52d0fcaf22f15d50bcdfbf672a82ffb7
Bertie <berniesnell@gmail.com>
2019-06-25 18:24:26 -0400

    can match against literals

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn match_against_literals() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
