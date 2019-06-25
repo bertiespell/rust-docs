f7132ffe3ad30dcc9af340100bd09b835b9a7280
Bertie <berniesnell@gmail.com>
2019-06-25 18:38:03 -0400

    can also destructure on an enum

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     match_range();
     destructuring();
     use_literal_destructuring();
+    destructure_enum();
 }
 
 fn match_against_literals() {
@@ -91,5 +92,38 @@ fn use_literal_destructuring() {
         Point { x, y: 0 } => println!("On the x axis at {}", x),
         Point { x: 0, y } => println!("On the y axis at {}", y), // we match this case!
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn destructure_enum() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        },
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y
+            );
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        }
     }
 }
