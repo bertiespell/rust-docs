15f3b0d38124b175cb7f0e7d0efcf86adad2d8e3
Bertie <berniesnell@gmail.com>
2019-06-25 18:28:55 -0400

    problem of shadow variables in pattern matching

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    match_names_variables();
 }
 
 fn match_against_literals() {
@@ -11,4 +11,18 @@ fn match_against_literals() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+}
+
+//  there is a complication when you use named variables in match expressions. Because match starts a new scope, variables declared as part of a pattern inside the match expression will shadow those with the same name outside the match construct, as is the case with all variables.
+fn match_names_variables() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y), // The pattern in the second match arm introduces a new variable named y that will match any value inside a Some value. Because we’re in a new scope inside the match expression, this is a new y variable, not the y we declared at the beginning with the value 10. This new y binding will match any value inside a Some, which is what we have in x. Therefore, this new y binds to the inner value of the Some in x. That value is 5, so the expression for that arm executes and prints Matched, y = 5.
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
