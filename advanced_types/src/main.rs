b3d9767f9d34819fe00a80e700072272e157aeda
Bertie <berniesnell@gmail.com>
2019-06-26 19:24:50 -0400

    type aliases

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -1,3 +1,23 @@
+/**
+ * Another use of the newtype pattern is in abstracting away some implementation details of a type: the new type can expose a public API that is different from the API of the private inner type if we used the new type directly to restrict the available functionality, for example.
+ * 
+ * Newtypes can also hide internal implementation. For example, we could provide a People type to wrap a HashMap<i32, String> that stores a person’s ID associated with their name. Code using People would only interact with the public API we provide, such as a method to add a name string to the People collection; that code wouldn’t need to know that we assign an i32 ID to names internally. The newtype pattern is a lightweight way to achieve encapsulation to hide implementation details
+ */
 fn main() {
-    println!("Hello, world!");
+    use_type_alias();
+}
+
+// Creating Type Synonyms with Type Aliases
+
+// Along with the newtype pattern, Rust provides the ability to declare a type alias to give an existing type another name. For this we use the type keyword. For example, we can create the alias Kilometers to i32 like so:
+
+type Kilometers = i32;
+
+fn use_type_alias() {
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
 }
