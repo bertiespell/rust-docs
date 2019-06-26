7d455852f26cc9aa407dbd1e1266f2eb0cae8573
Bertie <berniesnell@gmail.com>
2019-06-26 16:15:29 -0400

    Using associated types instead of generics

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -20,4 +20,11 @@ Associated types might seem like a similar concept to generics, in that the latt
 
 Let’s examine the difference between the two concepts
 
+The difference is that when using generics, as in Listing 19-21, we must annotate the types in each implementation; because we can also implement Iterator<String> for Counter or any other type, we could have multiple implementations of Iterator for Counter. In other words, when a trait has a generic parameter, it can be implemented for a type multiple times, changing the concrete types of the generic type parameters each time. When we use the next method on Counter, we would have to provide type annotations to indicate which implementation of Iterator we want to use.
+
+With associated types, we don’t need to annotate types because we can’t implement a trait on a type multiple times. In Listing 19-20 with the definition that uses associated types, we can only choose what the type of Item will be once, because there can only be one impl Iterator for Counter. We don’t have to specify that we want an iterator of u32 values everywhere that we call next on Counter.
  */
+
+pub trait Iterator2<T> {
+    fn next(&mut self) -> Option<T>;
+}
